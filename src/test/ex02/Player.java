34cc9fccff5f4a057418ccd97d539313812e3d33
YHYELIM <yhl0513@naver.com>
2023-06-19 17:49:29 +0900

    bubble game

---
+++ src/test/ex02/Player.java
@@ -0,0 +1,34 @@
+package test.ex02;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Player extends JLabel {
+
+    private int x;
+    private int y;
+
+    private ImageIcon playerR, playerL;
+
+    public Player() {
+        initObject();
+        initSetting();
+
+    }
+
+    // 오브젝트 먼저 하고 셋팅
+    public void initObject() {
+        playerR = new ImageIcon("image/playerR.png");
+        playerL = new ImageIcon("image/playerL.png");
+
+    }
+
+    private void initSetting() {
+        x = 70;
+        y = 535;
+
+        setIcon(playerR);
+        setSize(50, 50);
+        setLocation(x, y);
+    }
+}
